6c25e3f2bd5c3d3fab4a72846c81e69b94b12a2f
Manik <maniksingh3606@gmaile.com>
2025-09-06 09:37:44 +0530

    all files addedd

---
+++ src/components/ui/spline-element.tsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function SplineElement() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/zxPyrPOzBLAHVVaQ/scene.splinecode" 
+      />
+    </main>
+  );
+}
